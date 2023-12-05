9c354cf8bb7d5e089c82c4bb26f26c093c817012
maartaperez <152414556+maartaperez@users.noreply.github.com>
2023-12-06 00:40:46 +0100

    Update SqlBases EDITADO HASTA PRODUCTOS VETERINARIOS(NO INCLUIDO).sql

--- SqlBases.sql
+++ SqlBases.sql
@@ -1,128 +1,157 @@
 ﻿CREATE TABLE Entidad_reguladora
 (
-  CodEntidad NUMBER NOT NULL,
-  PRIMARY KEY (CodEntidad)
+  CodEntidad NUMBER(9),
+  CONSTRAINT PK_Entidad_reguladora PRIMARY KEY (CodEntidad)
 );
 
 CREATE TABLE Gestor_responsable
 (
-  NIF NUMBER NOT NULL,
+  NIF VARCHAR2(9),
   Nombre VARCHAR2(50) NOT NULL,
   Apellido VARCHAR2(50) NOT NULL,
-  DiaNacimiento NUMBER NOT NULL,
-  MesNacimiento NUMBER NOT NULL,
-  AnoNacimiento NUMBER NOT NULL,
+  DiaNacimiento NUMBER(2) NOT NULL,
+  MesNacimiento NUMBER(2) NOT NULL,
+  AnoNacimiento NUMBER(4) NOT NULL,
   Titulacion VARCHAR2(100) NOT NULL,
-  Correo_electronico VARCHAR2(100) NOT NULL,
-  Telefono_urgencia VARCHAR2(15) NOT NULL,
-  PRIMARY KEY (NIF)
+  Correo_electronico VARCHAR2(100) NOT NULL UNIQUE,
+  Telefono_urgencia VARCHAR2(15) NOT NULL UNIQUE,
+  CONSTRAINT PK_Gestor_responsable PRIMARY KEY (NIF),
+  CONSTRAINT CHK_DiaNacimiento CHECK(DiaNacimiento < SYSDATE),
+  CONSTRAINT CHK_MesNacimiento CHECK(MesNacimiento < SYSDATE),
+  CONSTRAINT CHK_AnoNacimiento CHECK(AnoNacimiento < SYSDATE)
 );
 
 CREATE TABLE Granja
 (
-  GPS VARCHAR2(50) NOT NULL,
-  Numero VARCHAR2(10) NOT NULL,
+  NIF_titular VARCHAR2(9),
+  GPS VARCHAR2(50) NOT NULL UNIQUE,
+  Numero NUMBER(10) NOT NULL UNIQUE,
   Calle VARCHAR2(100) NOT NULL,
-  NIF_titular NUMBER NOT NULL,
-  PRIMARY KEY (NIF_titular)
+  CONSTRAINT PK_Granja PRIMARY KEY (NIF_Titular),
+  CONSTRAINT CHK_Numero CHECK(Numero > 0)
 );
 
 CREATE TABLE Fabrica
 (
-  GPS VARCHAR2(50) NOT NULL,
+  NIF_empresa VARCHAR2(9),
+  GPS VARCHAR2(50) NOT NULL UNIQUE,
   Calle VARCHAR2(100) NOT NULL,
-  Numero VARCHAR2(10) NOT NULL,
-  NIF_empresa NUMBER NOT NULL,
-  Telefono_urgencias VARCHAR2(15) NOT NULL,
-  Mail VARCHAR2(100) NOT NULL,
-  CodRegistroSanitario VARCHAR2(50) NOT NULL,
-  DiaFabrica NUMBER NOT NULL,
-  MesFabrica NUMBER NOT NULL,
-  AnoFabrica NUMBER NOT NULL,
-  Cantidad_proc_dia NUMBER NOT NULL,
-  Cant_proc_mes NUMBER NOT NULL,
-  Cant_proc_ano NUMBER NOT NULL,
-  PRIMARY KEY (NIF_empresa)
+  Numero NUMBER(10) NOT NULL UNIQUE,
+  Telefono_urgencias VARCHAR2(15) NOT NULL UNIQUE,
+  Mail VARCHAR2(100) NOT NULL UNIQUE,
+  CodRegistroSanitario VARCHAR2(50) NOT NULL UNIQUE,
+  DiaFabrica NUMBER(2) NOT NULL,
+  MesFabrica NUMBER(2) NOT NULL,
+  AnoFabrica NUMBER(4) NOT NULL,
+  Cantidad_proc_dia NUMBER(100),
+  Cant_proc_mes NUMBER(100),
+  Cant_proc_ano NUMBER(100),
+  CONSTRAINT PK_Fabrica PRIMARY KEY (NIF_empresa),
+  CONSTRAINT CHK_Numero CHECK(Numero > 0),
+  CONSTRAINT CHK_DiaFabrica CHECK(DiaFabrica < SYSDATE),
+  CONSTRAINT CHK_MesFabrica CHECK(MesFabrica < SYSDATE),
+  CONSTRAINT CHK_AnoFabrica CHECK(AnoFabrica < SYSDATE),
+  CONSTRAINT CHK_Cant_proc_dia CHECK(Cant_proc_dia >= 0),
+  CONSTRAINT CHK_Cant_proc_mes CHECK(Cant_proc_mes >= 0),
+  CONSTRAINT CHK_Cant_proc_ano CHECK(Cant_proc_ano >= 0)
 );
 
 CREATE TABLE Quimicos
 (
-  Cod_quimico NUMBER NOT NULL,
-  Cant NUMBER NOT NULL,
-  DiaQuimico NUMBER NOT NULL,
-  MesQuimico NUMBER NOT NULL,
-  AnoQuimico NUMBER NOT NULL,
-  PRIMARY KEY (Cod_quimico)
+  Cod_quimico NUMBER(9),
+  Cant NUMBER(100),
+  DiaQuimico NUMBER(2) NOT NULL,
+  MesQuimico NUMBER(2) NOT NULL,
+  AnoQuimico NUMBER(2) NOT NULL,
+  CONSTRAINT PK_Quimicos PRIMARY KEY (Cod_Quimico),
+  CONSTRAINT CHK_DiaQuimico CHECK(DiaQumico < SYSDATE),
+  CONSTRAINT CHK_MesQuimico CHECK(MesQumico < SYSDATE),
+  CONSTRAINT CHK_AnoQumico CHECK(AnoQumico < SYSDATE),
+  CONSTRAINT CHK_Cant CHECK(Cant >= 0)
 );
 
 CREATE TABLE Destinatario
 (
-  NIF NUMBER NOT NULL,
+  NIF VARCHAR2(9),
   Calle VARCHAR2(100) NOT NULL,
-  Numero VARCHAR2(10) NOT NULL,
-  Web VARCHAR2(100) NOT NULL,
-  Correo_electronico VARCHAR2(100) NOT NULL,
-  Telefono VARCHAR2(15) NOT NULL,
-  Telefono_urgencia VARCHAR2(15) NOT NULL,
-  PRIMARY KEY (NIF)
+  Numero NUMBER(10) NOT NULL UNIQUE,
+  Web VARCHAR2(100) UNIQUE,
+  Correo_electronico VARCHAR2(100) NOT NULL UNIQUE,
+  Telefono NUMBER(15) NOT NULL UNIQUE,
+  Telefono_urgencia NUMBER(15) NOT NULL UNIQUE,
+  CONSTRAINT PK_Destinatario PRIMARY KEY (NIF),
+  CONSTRAINT CHK_Numero CHECK(Numero > 0)
 );
 
 CREATE TABLE Empresa_logistica
 (
-  NIF NUMBER NOT NULL,
-  Denominacion VARCHAR2(100) NOT NULL,
+  NIF VARCHAR2(9),
+  Denominacion VARCHAR2(100) NOT NULL UNIQUE,
   Calle VARCHAR2(100) NOT NULL,
-  Numero VARCHAR2(10) NOT NULL,
-  CodAutorizacionMercan VARCHAR2(50) NOT NULL,
-  CodAutorizacionAlim VARCHAR2(50) NOT NULL,
-  DiaLogistica NUMBER NOT NULL,
-  MesLogistica NUMBER NOT NULL,
-  AnoLogistica NUMBER NOT NULL,
-  NIF_responsable NUMBER NOT NULL,
-  PRIMARY KEY (NIF)
+  Numero NUMBER(10) NOT NULL UNIQUE,
+  CodAutorizacionMercan VARCHAR2(50) NOT NULL UNIQUE,
+  CodAutorizacionAlim VARCHAR2(50) NOT NULL UNIQUE,
+  DiaLogistica NUMBER(2) NOT NULL,
+  MesLogistica NUMBER(2) NOT NULL,
+  AnoLogistica NUMBER(4) NOT NULL,
+  NIF_responsable VARCHAR(9) NOT NULL UNIQUE,
+  CONSTRAINT PK_Empresa_logistica PRIMARY KEY (NIF),
+  CONSTRAINT CHK_Numero CHECK(Numero > 0),
+  CONSTRAINT CHK_DiaLogistica CHECK(DiaFabrica < SYSDATE),
+  CONSTRAINT CHK_MesLogistica CHECK(MesFabrica < SYSDATE),
+  CONSTRAINT CHK_AnoLogistica CHECK(AnoFabrica < SYSDATE)
 );
 
 CREATE TABLE Envases
 (
-  NIF_fabricante NUMBER NOT NULL,
-  CodEnvase NUMBER NOT NULL,
-  Factor_resistencia_acidos NUMBER NOT NULL,
-  Opacidad NUMBER NOT NULL,
+  CodEnvase NUMBER(9),
+  NIF_fabricante VARCHAR2(9) NOT NULL,
+  Factor_resistencia_acidos VARCHAR2(100) NOT NULL,
+  Opacidad VARCHAR2(50) NOT NULL,
   Tipo_reciclaje VARCHAR2(50) NOT NULL,
-  PRIMARY KEY (CodEnvase)
+  CONSTRAINT PK_Envases PRIMARY KEY (CodEnvase)
 );
 
 CREATE TABLE Alerta
 (
-  CodAlerta NUMBER NOT NULL,
+  CodAlerta NUMBER(9),
   Tipo VARCHAR2(50) NOT NULL,
-  DiaAlerta NUMBER NOT NULL,
-  MesAlerta NUMBER NOT NULL,
-  AnoAlerta NUMBER NOT NULL,
-  CodProducto NUMBER NOT NULL,
-  CodProductor NUMBER NOT NULL,
-  DiaIntervencion NUMBER NOT NULL,
-  MesIntervencion NUMBER NOT NULL,
-  AnoIntervencion NUMBER NOT NULL,
+  DiaAlerta NUMBER(2) NOT NULL,
+  MesAlerta NUMBER(2) NOT NULL,
+  AnoAlerta NUMBER(4) NOT NULL,
+  CodProducto NUMBER(9) NOT NULL UNIQUE,
+  CodProductor NUMBER(9) NOT NULL,
+  DiaIntervencion NUMBER(2) NOT NULL,
+  MesIntervencion NUMBER(2) NOT NULL,
+  AnoIntervencion NUMBER(4) NOT NULL,
   Intervencion VARCHAR2(100) NOT NULL,
-  PRIMARY KEY (CodAlerta)
+  CONSTRAINT PK_Alerta PRIMARY KEY (CodAlerta),
+  CONSTRAINT CHK_DiaAlerta CHECK(DiaAlerta < SYSDATE),
+  CONSTRAINT CHK_MesAlerta CHECK(MesAlerta < SYSDATE),
+  CONSTRAINT CHK_AnoAlerta CHECK(AnoAlerta < SYSDATE),
+  CONSTRAINT CHK_DiaIntervencion CHECK(DiaIntervencion < SYSDATE),
+  CONSTRAINT CHK_MesIntervencion CHECK(MesIntervencion < SYSDATE),
+  CONSTRAINT CHK_AnoIntervencion CHECK(AnoIntervencion < SYSDATE)
 );
 
 CREATE TABLE Fertilizantes_y_Agroquimicos
 (
-  Cod_Producto NUMBER NOT NULL,
-  Denominacion VARCHAR2(100) NOT NULL,
+  Cod_Producto NUMBER(9),
+  Denominacion VARCHAR2(100) NOT NULL UNIQUE,
   Fabricante VARCHAR2(100) NOT NULL,
-  Cant_Diaria NUMBER NOT NULL,
-  Cant_Media_Semanal NUMBER NOT NULL,
-  Cant_Media_Mensual NUMBER NOT NULL,
-  NIF_Responsable NUMBER NOT NULL,
-  PRIMARY KEY (Cod_Producto)
+  Cant_Diaria NUMBER(100),
+  Cant_Media_Semanal NUMBER(100),
+  Cant_Media_Mensual NUMBER(100),
+  NIF_Responsable VARCHAR2(9) NOT NULL,
+  CONSTRAINT PK_Fertilizantes_y_Agroquimicos PRIMARY KEY (Cod_Producto),
+  CONSTRAINT CHK_Cant_Diaria CHECK(Cant_Diaria >= 0),
+  CONSTRAINT CHK_Cant_Media_Semanal CHECK(Cant_Media_Semanal >= 0),
+  CONSTRAINT CHK_Cant_Mensual CHECK(Cant_Mensual >= 0)
 );
 
 CREATE TABLE Productos_Veterinarios_y_Vacunas
 (
-  Cod_Medicamento NUMBER NOT NULL,
+  Cod_Medicamento NUMBER(9),
   Cod_Fabricante NUMBER NOT NULL,
   DiaMedicamento NUMBER NOT NULL,
   MesMedicamento NUMBER NOT NULL,
